aa98f33d4f903fe636be9496f9ca77d90384dd70
Airthon Darle <airthondarle@MacBook-Air-de-Airthon.local>
2022-03-07 17:53:44 -0300

    first commit

---
+++ funcos/arrowFunciton3.js
@@ -0,0 +1,19 @@
+let comparaComThis = function (param) {
+    console.log(this === param)
+}
+
+
+comparaComThis(global)
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj)
+comparaComThis(global)
+comparaComThis(obj)
+
+let comparaComThisarrow = param => console.log(this === param)
+comparaComThisarrow(global)
+comparaComThisarrow(module.exports)
+
+comparaComThisarrow = comparaComThisarrow.bind(obj)
+comparaComThisarrow(obj)
+comparaComThisarrow(module.exports)
