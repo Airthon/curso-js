fb3ade57412f848f4828064eee61d7c4fb5cebc7
Airthon Darle <airthondarle@MacBook-Air-de-Airthon.local>
2022-03-08 23:11:21 -0300

    first commit

---
+++ Objetos/oo.js
@@ -0,0 +1,22 @@
+// código não execultado
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+Object = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+
+Object.processamento() // Foco passou a ser o objeto
+
+// Principios importantes:
+// 1. abstracao
+// 2. encapsulamento
+// 3. herança (prototype)
+// 4. polimorfismo
