2382a0eb1b4c9005193d995ba029b34d22e86a1b
Darle <ayrtomdarly@gmail.com>
2022-03-11 13:46:34 -0300

    master

--- funcos/arrowFunction1.js
+++ funcos/arrowFunction1.js
@@ -2,9 +2,9 @@ let dobro = function (a){
     return 2 * a
 }
 
-dobro = (a) => {
-    return 2 * a
-} 
+                                dobro = (a) => {
+                                    return 2 * a
+                } 
 
 dobro = a => 2 * a
 console.log (dobro(Math.PI))
