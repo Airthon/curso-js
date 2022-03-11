09ff81f03583f2feca9dbf9ab204653c4601160b
Darle <ayrtomdarly@gmail.com>
2022-03-11 16:50:59 -0300

    master

---
+++ funcoesImportantesObjeto.js
@@ -0,0 +1,14 @@
+const pessoa = {
+    nome: 'Darle',
+    idade: 21,
+    pesa: 85
+}
+
+console.log(Object.keys(pessoa))
+console.log(Object.values(pessoa))
+console.log(Object.entries(pessoa))
+
+Object.entries(pessoa).forEach(([chave, valor]) => {
+    console.log(`${chave}: ${valor}`)
+})
+
