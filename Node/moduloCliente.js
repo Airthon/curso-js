da8fb4617bf999de8a633a2b8a795da061e02831
Darle <ayrtomdarly@gmail.com>
2022-03-21 23:30:53 -0300

    master

---
+++ Node/moduloCliente.js
@@ -0,0 +1,8 @@
+const moduloA = require('./modolua')
+const moduloB = require('./modulob')
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.console)
+
+console.log(moduloB)
